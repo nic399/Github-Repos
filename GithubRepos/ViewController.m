5d63311513dc470379bbb57cd338497294ba14e7
Nicholas Fung <nbfung@sfu.ca>
2017-10-23 12:12:47 -0700

    Download and display repo names in tableView

--- GithubRepos/ViewController.m
+++ GithubRepos/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong, readwrite) NSMutableArray *repoArr;
 
 @end
 
@@ -17,6 +19,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.repoArr = [NSMutableArray new];
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.github.com/users/nic399/repos"]; // 1
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url]; // 2
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 3
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 4
+    
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) { // 1
+            // Handle the error
+            NSLog(@"error: %@", error.localizedDescription);
+            return;
+        }
+        
+        NSError *jsonError = nil;
+        NSArray *repos = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError]; // 2
+        
+        if (jsonError) { // 3
+            // Handle the error
+            NSLog(@"jsonError: %@", jsonError.localizedDescription);
+            return;
+        }
+        
+        // If we reach this point, we have successfully retrieved the JSON from the API
+        for (NSDictionary *repo in repos) { // 4
+            [self.repoArr addObject:repo];
+            NSString *repoName = repo[@"name"];
+            NSLog(@"repo: %@", repoName);
+        }
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            // This will run on the main queue
+            [self.tableView reloadData];
+        }];
+        
+    }]; // 5
+    
+    [dataTask resume]; // 6
+    
 }
 
 
@@ -25,5 +69,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return self.repoArr.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"myCell"];
+    
+    cell.textLabel.text = self.repoArr[indexPath.row][@"name"];
+    return cell;
+}
 
 @end
